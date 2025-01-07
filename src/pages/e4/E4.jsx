9f0bc918903ed6de5006c80a676b0974bff8f0de
Santiago <snaranjograjales@gmail.com>
2025-01-07 02:38:56 +0100

    font-size: 30px;

--- src/pages/e4/E4.jsx
+++ src/pages/e4/E4.jsx
@@ -1,3 +1,4 @@
+import "@react-pdf-viewer/core/lib/styles/index.css"; // Import des styles par défaut
 import "./E4.css";
 
 const E4 = () => {
@@ -6,22 +7,43 @@ const E4 = () => {
       <h1>Épreuve E4</h1>
       <p>
         Cette page est dédiée à l&apos;épreuve E4 du BTS SIO. Elle présente les
+        <br></br>
         projets développés, les technologies utilisées et les compétences mises
+        <br></br>
         en avant.
       </p>
-      <div className="list-e4-container">
-        <h2>Projets présentés</h2>
-        <ul>
-          <li>Projet 1 : Gestion de stock (Java, MySQL)</li>
-          <li>Projet 2 : Application mobile de réservation (React Native)</li>
-        </ul>
-        <h2>Technologies utilisées</h2>
-        <ul>
-          <li>Java</li>
-          <li>MySQL</li>
-          <li>React Native</li>
-          <li>Node.js</li>
-        </ul>
+      {/* Section Rapport de Stage */}
+      <div className="section">
+        <h2>Rapport de Stage</h2>
+        <iframe
+          src="/Rapport_de_stage_20232024.pdf"
+          title="Rapport de Stage"
+          width="50%"
+          height="1000px"
+          style={{ border: "none" }}
+        ></iframe>
+        {/* Section Tableau de Synthèse */}
+        <h2>Tableau de Synthèse</h2>
+        <p>
+          Le tableau de synthèse est un outil essentiel dans le cadre de
+          <br></br>
+          l&apos;épreuve E4 du BTS SIO. Son objectif principal est de fournir
+          une vue
+          <br></br>
+          d&apos;ensemble des compétences mises en œuvre lors des projets ou
+          <br></br>
+          activités réalisés par le candidat au cours de sa formation et, en
+          <br></br>
+          particulier, durant son stage. Ce tableau joue un rôle clé dans
+          <br></br>
+          l&apos;évaluation de la capacité du candidat à mobiliser les
+          compétences
+          <br></br>
+          techniques et organisationnelles acquises.
+        </p>
+        <a href="/TableauSyntheseE4.xlsx" download className="download-link">
+          Télécharger le Tableau de Synthèse
+        </a>
       </div>
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,13 @@ const Home = () => {
   return (
     <div className="home-container">
       <h1>Bienvenue sur mon portfolio</h1>
-      <p>Ceci est la page d&apos;accueil.</p>
+      <p>
+        Je m&apos;appelle Santiago Naranjo Grajales et je suis étudiant en BTS
+        <br></br>
+        SIO (Services Informatiques aux Organisations) option SLAM (Solutions
+        <br></br>
+        Logicielles et Applications Métiers)
+      </p>
     </div>
   );
 };
